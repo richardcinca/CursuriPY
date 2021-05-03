5c5b4a295b32f811f5419332f42ac601d753fa59
unknown <richard.cinca@gmail.com>
2021-05-03 15:13:01 +0300

    Exercitiul 2

---
+++ ex2.py
@@ -0,0 +1,12 @@
+x=10
+while x>1:
+    print("X este {}".format(x))
+    pass
+    x=x-1
+
+y=int(input("Insert value:"))
+while y>1:
+    print("Y este {}".format(y))
+    break
+    #continue
+    y=y-1
