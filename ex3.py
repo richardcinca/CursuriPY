a1f18c074598dea4ae6337ba0adde6ab4dc508ad
unknown <richard.cinca@gmail.com>
2021-05-03 15:41:43 +0300

    Exercitiul 3 - Iterators

---
+++ ex3.py
@@ -0,0 +1,11 @@
+word = input("Insert word: ")
+for i in word:
+    print(i)
+
+for n in range(int(input("insert range"))):
+    print(n)
+
+print(".....................................................")
+
+for m in range(0,20,2):
+    print(m)
