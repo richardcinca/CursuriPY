00dc4a75df1abf31a28cf0ede388c119bebd179d
unknown <richard.cinca@gmail.com>
2021-05-04 07:16:43 +0300

    String methods

---
+++ Strings.py
@@ -0,0 +1,15 @@
+x="merry christmas"
+y=x.replace("m","b")
+
+print(y)
+
+#FIND METHOD
+string="abcdef"
+print(string.find("de"))
+
+#SPLIT METHOD
+text="Good morning"
+print(text.split(" "))
+
+y = "sir de caractere"
+print("y.split('') {}".format(y.split(" ")))
